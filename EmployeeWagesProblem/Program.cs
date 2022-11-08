961683843a0006b256d2aebb9db7ad21f1c61e44
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-11-08 21:58:26 +0530

    [sourav]add : welcome message

--- EmployeeWagesProblem/Program.cs
+++ EmployeeWagesProblem/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeWagesProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wages Problem");
+        }
+    }
+}
